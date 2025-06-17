2ec3a3f546ae43e188d1e0646fbdcabe49fae658
sajin-oops <sajinsheeba2003@gmail.com>
2025-06-17 11:30:05 +0530

    feat : created a dataframe using pandas

---
+++ pandas.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.DataFrame({
+    'Employees': ["sajin","lisa","sara","john"],
+    'Salary': [1000, 1500, 2000, 2500],
+    'Department': ["HR", "Finance", "IT", "Marketing"]
+})
